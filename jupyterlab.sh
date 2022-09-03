bf4e29451b7ec0b856b612ad5d3239f36b844518
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-09-03 15:49:18 +0800

    update

--- jupyterlab.sh
+++ jupyterlab.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-jupyter lab --notebook-dir=/home/hadoop/notebooks --ip='*' --port=27649 --no-browser  
 sudo chown -R hadoop:hadoop notebooks
+
+jupyter lab --notebook-dir=/home/hadoop/notebooks --ip='*' --port=27649 --no-browser  
