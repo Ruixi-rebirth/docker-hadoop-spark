472a5bef29242f31537e11ced8e36129ab5da00c
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-09-04 15:21:41 +0800

    Update misc.sh

--- misc.sh
+++ misc.sh
@@ -6,8 +6,8 @@ sudo chown -R hadoop:hadoop notebooks
 #因为这两个文件名和/home/hadoop/hadoop/etc/hadoop 下的文件重名了, 需要重命名一下
 original_start_file="/home/hadoop/spark/sbin/start-all.sh"
 modified_start_file="/home/hadoop/spark/sbin/start-all-spark.sh"
-original_start_file="/home/hadoop/spark/sbin/stop-all.sh"
-modified_start_file="/home/hadoop/spark/sbin/stop-all-spark.sh"
+original_stop_file="/home/hadoop/spark/sbin/stop-all.sh"
+modified_stop_file="/home/hadoop/spark/sbin/stop-all-spark.sh"
 if [ -f $original_start_file ]; then 
   mv $original_start_file $modified_start_file
 fi 
